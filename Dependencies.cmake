95976f5685cbd8d8fd151d4fec17e337c4fb4795
AlenDio0 <alendio.0000@gmail.com>
2025-11-24 21:33:26 +0100

    Added yaml-cpp with CMake

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -11,4 +11,23 @@ FetchContent_Declare(
     EXCLUDE_FROM_ALL
     SYSTEM
 )
+
 FetchContent_MakeAvailable(SFML)
+
+# yaml-cpp
+FetchContent_Declare(
+    yaml-cpp
+    GIT_REPOSITORY https://github.com/jbeder/yaml-cpp.git
+    GIT_TAG a83cd31
+    GIT_SHALLOW ON
+    EXCLUDE_FROM_ALL
+)
+set(YAML_CPP_BUILD_TOOLS    OFF)
+set(YAML_CPP_BUILD_CONTRIB  OFF)
+
+set(CMAKE_FOLDER "yaml-cpp" CACHE INTERNAL "IDE Folder for yaml-cpp")
+
+FetchContent_MakeAvailable(yaml-cpp)
+
+# Unload Custom IDE Folder
+unset(CMAKE_FOLDER CACHE)
